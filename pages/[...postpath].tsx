dbe1620c13ea8202095968e35de10ee27cb2774a
Featureposts <133018251+Featureposts@users.noreply.github.com>
2023-05-22 04:24:06 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.highrevenuegate.com/g02dgsu0b6?key=7421a7cc705cdf79170a437c3d359057` 
+					`https://fea-guru.vercel.app/fc-krasnodar` 
 				}`,
 			},
 		};
