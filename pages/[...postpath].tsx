b81b294f084fcb18449c096f0b6528158ab967cb
Featureposts <133018251+Featureposts@users.noreply.github.com>
2023-05-27 18:16:31 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://fea-guru.vercel.app/fc-krasnodar` 
+					`https://featureposts.com` 
 				}`,
 			},
 		};
